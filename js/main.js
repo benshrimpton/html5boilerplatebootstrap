a60de3e83983654f57ea121ef88018fa889975cb
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2016-04-19 18:53:29 -0400

    Toggle video playback on nav interaction

--- js/main.js
+++ js/main.js
@@ -5,19 +5,33 @@ if ($(window).width() <= 800 ){
 
 $('.hamburger').on('click', function(){
   $('body').toggleClass('nav-open');
+  toggleVid();
 });
+
+function toggleVid(){
+  if( $('body').hasClass('nav-open') ){
+    $('video')[0].pause()
+  }
+  else {
+    $('video')[0].play()
+  }
+}
 
 $('.menu a').on('click', function(e){
   e.preventDefault();
   cta = $('.cta')
   text = $(this).data('text')
+
   setTimeout(function () {
     $('body').toggleClass('nav-open');
     if (text != ""){
       cta.text(text);
+      $('video')[0].play()
     }
     else {
       cta.text("Coming soon...");
+      $('video')[0].play()
+
     }
 
   }, 300);
