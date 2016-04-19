89f6b2cc7acf1f9004c7d5afa56e9b03b287215d
benshrimpton <benjaminshrimpton@yahoo.co.uk>
2016-04-19 18:27:05 -0400

    Add a sidebar nav

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ if ($(window).width() <= 800 ){
   $(video).remove();
   $('body').append('<div class="gif-wrap"></div>');
 }
+
+$('.hamburger').on('click', function(){
+  $('body').toggleClass('nav-open')
+});
